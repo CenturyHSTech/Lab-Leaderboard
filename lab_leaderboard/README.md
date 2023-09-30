073c6925688801b6a7daa8c5c673dbd720b3bafe
Hundredvisionsguy <cwinikka@gmail.com>
2023-09-29 17:20:09 -0700

    setting up initial files

---
+++ lab_leaderboard/README.md
@@ -0,0 +1,7 @@
+#Lab-Leaderboard
+
+## From [Custom Scrolling Quote Board Matrix Display](https://learn.adafruit.com/aio-quote-board-matrix-display)
+By [John Park](https://learn.adafruit.com/u/johnpark)
+
+## TODO:
+Let's get some students involved in this.
---
+++ lab_leaderboard/leaderboard.py
@@ -0,0 +1,13 @@
+"""This is the script that will perform all the checks on day and time
+and determine how much time is left in a class based on our schedule.
+
+It will also change the background color based on whether students can
+use a hall pass or not"""
+
+# imports
+
+# define functions
+
+
+if __name__ == "__main__":
+    print("Here's where we'll test our code.")
