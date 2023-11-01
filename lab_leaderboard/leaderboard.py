cd5df922bec281dbbf45882714efc6489dacd1fe
AdamAinsworthCHS <ainsa128@hsd.k12.or.us>
2023-11-01 15:19:14 -0700

    Added a function that tells you the current schedule

--- lab_leaderboard/leaderboard.py
+++ lab_leaderboard/leaderboard.py
@@ -25,9 +25,62 @@ MONDAY = {
     "13:18-13:52": ("south", "lunch"),
     "13:52-15:30": ("both", "class", "4th", "8th"),
 }
+TUESDAY = {
+    "08:30-9:54": ("both", "class", "1st", "5th"),
+    "10:00-11:24": ("both", "class", "2nd", "6th"),
+    "11:30-12:00": ("both", "advisory"),
+    "12:00-12:30": ("north", "lunch"),
+    "12:06-13:30": ("south", "class", "3rd", "7th"),
+    "12:36-14:00": ("north", "class", "3rd", "7th"),
+    "13:30-14:00": ("south", "lunch"),
+    "14:06-15:30": ("both", "class", "4th", "8th"),
+}
+WEDNESDAY = {
+    "9:00-10:26": ("both", "class", "1st", "5th"),
+    "10:32-11:58": ("both", "class", "2nd", "6th"),
+    "11:58-12:28": ("north", "lunch"),
+    "12:04-13:29": ("south", "class", "3rd", "7th"),
+    "12:34-13:59": ("north", "class", "3rd", "7th"),
+    "13:29-13:59": ("south", "lunch"),
+    "14:05-15:30": ("both", "class", "4th", "8th"),
+}
+THURSDAY = {
+    "08:30-9:49": ("both", "class", "1st", "5th"),
+    "9:55-11:14": ("both", "class", "2nd", "6th"),
+    "11:20-12:05": ("both", "access/jagtime"),
+    "12:05-12:40": ("north", "lunch"),
+    "12:11-13:30": ("south", "class", "3rd", "7th"),
+    "12:46-14:05": ("north", "class", "3rd", "7th"),
+    "13:30-14:05": ("south", "lunch"),
+    "14:11-15:30": ("both", "class", "4th", "8th"),
+}
+FRIDAY = THURSDAY
 
 
 # define functions
+def get_schedule():
+    """
+    returns which schedule should be displayed
+    """
+    today = date.today()
+    weekday = today.weekday()
+    if weekday == 0:
+        schedule = MONDAY
+    elif weekday == 1:
+        schedule = TUESDAY
+    elif weekday == 2:
+        schedule = WEDNESDAY
+    elif weekday == 3:
+        schedule = THURSDAY
+    elif weekday == 4:
+        schedule = FRIDAY
+    elif weekday == 5:
+        schedule = "No school today!"
+    else:
+        schedule = "No school today!"
+    return schedule
+
+
 def get_time() -> tuple:
     """returns a tuple to indicate the day, A Day or B Day, hour and minute
 
@@ -35,11 +88,12 @@ def get_time() -> tuple:
         time: a dictionary with 'day' and 'AorB' keys as strings and 'hour'
             and 'min' as ints.
     """
+    now = datetime.now()
     time = {
-        "day": "",
-        "AorB": "",
-        "hour": 0,
-        "min": 0,
+        "day": now.day,
+        "AorB": results,
+        "hour": now.hour,
+        "min": now.minute,
     }
     return time
 
@@ -103,4 +157,5 @@ def get_chs_events() -> list:
 if __name__ == "__main__":
     print("Here's where we'll test our code.")
     results = get_chs_events()
-    print(results)
+    print(get_time())
+    print(get_schedule())
